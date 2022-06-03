5cc032e3bab895e95e3d78bd235758ff703b04b6
dongtimes2 <dongtimes2@gmail.com>
2022-06-04 03:10:27 +0900

    feat: react-router 설치 및 초기 구조 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+import Home from './pages/Home';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
