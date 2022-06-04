da2a5d70c8d4c13c81634e7e847831fb121b123a
dongtimes2 <dongtimes2@gmail.com>
2022-06-05 08:38:42 +0900

    feat: 로그인 한 유저의 토큰 정보가 axios의 req 헤더에 담기도록 구현함. 또한 로그인을 수행할 때마다 서버에게 로그인 post 요청이 가도록 구현함

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+});
+
+export const setAxios = async (user) => {
+  const token = await user?.getIdToken();
+  instance.defaults.headers.common['authorization'] = token;
+};
+
+export const getAxios = (url) => {
+  return instance.get(url);
+};
+
+export const postAxios = (url, data) => {
+  return instance.post(url, data);
+};
