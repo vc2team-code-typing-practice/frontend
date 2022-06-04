f8ac122fc692efa60259a3936d7852d28db62f69
dongtimes2 <dongtimes2@gmail.com>
2022-06-04 19:53:03 +0900

    feat: 로그아웃 버튼 컴포넌트 추가 및 버튼들 간 토글 기능 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-import GoogleLoginButton from '../components/GoogleLoginButton';
+import { useSelector } from 'react-redux';
+
+import LoginButton from '../components/LoginButton';
+import LogoutButton from '../components/LogoutButton';
 
 export default function Home() {
+  const isLogined = useSelector((state) => state.user.isLogined);
+
   return (
     <div>
       <div>
         <h1>쉽게 즐기는 코딩 타자 연습</h1>
       </div>
-      <div>
-        <GoogleLoginButton />
-      </div>
+      <div>{!isLogined ? <LoginButton /> : <LogoutButton />}</div>
     </div>
   );
 }
