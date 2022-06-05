fe405c4504bc4d26c4ef3a768c1ca10c1d2caa4c
rkdalstjd224 <rkdalstjd224@gmail.com>
2022-06-05 19:52:23 +0900

    feat: 로고아이콘 사용을 위해 material icons설치, sidebar menu에 사용

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,50 +1,47 @@
 import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import KeyboardIcon from '@mui/icons-material/Keyboard';
+import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
 import { Link } from 'react-router-dom';
 
-import { login, logout } from '../features/userSlice';
-
-import Button from './Button';
+import './Menu.scss';
 
 export default function Menu() {
   const items = [
     {
       path: '/',
       text: 'Home',
+      logo: <MapsHomeWorkIcon />,
     },
     {
-      path: '/about',
-      text: 'About',
+      path: '/word',
+      text: 'Word',
+      logo: <KeyboardIcon />,
     },
     {
-      path: '/contact',
-      text: 'Contact',
+      path: '/sentence',
+      text: 'Sentence',
+      logo: <KeyboardIcon />,
+    },
+    {
+      path: '/paragraph',
+      text: 'Paragraph',
+      logo: <KeyboardIcon />,
     },
   ];
-  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-  const dispatch = useDispatch();
-  const handleLoginButtonClick = () => {
-    dispatch(login());
-  };
-  const handleLogoutButtonClick = () => {
-    dispatch(logout());
-  };
 
   return (
     <div>
       <ul className="menu">
         {items.map((item) => (
           <li key={item.path} className="menu__item">
-            <Link to={item.path}>{item.text}</Link>
+            <Link to={item.path}>
+              {item.logo}
+              {item.text}
+            </Link>
           </li>
         ))}
       </ul>
-      {!isLoggedIn ? (
-        <Button onClick={handleLoginButtonClick}>구글로 로그인</Button>
-      ) : (
-        <Button onClick={handleLogoutButtonClick}>로그아웃</Button>
-      )}
     </div>
   );
 }
