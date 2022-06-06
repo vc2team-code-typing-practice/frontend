e8a6e093289651acaa14c518eae93cbb46143401
dongtimes2 <dongtimes2@gmail.com>
2022-06-06 13:37:17 +0900

    feat: redux 로직 수정. 먼저 상태관리하는 유저의 정보를 늘렸음. 이를 위해서는 유저 정보를 가져올 때 saga 함수가 db에 저장된 유저 정보를 가져올 수 있도록 해야 했고. 그렇게 동작하도록 구현함. 또 유저정보 페이지에서 유저가 환경설정을 변경할 때 변경된 정보가 우선 redux에 저장되고 이후 saga 함수를 통해 db에도 저장될 수 있도록 구현함

--- src/features/userSaga.js
+++ src/features/userSaga.js
@@ -1,18 +1,47 @@
 import { put, takeLatest, call } from 'redux-saga/effects';
 
-import { postAxios } from '../api';
+import { postAxios, getAxios, patchAxios } from '../api';
 import { authService, firebaseInstance } from '../auth';
 
-import { login, loginSuccess, logout, logoutSuccess } from './userSlice';
+import {
+  login,
+  loginSuccess,
+  logout,
+  logoutSuccess,
+  changeSetting,
+} from './userSlice';
 
 const provider = new firebaseInstance.auth.GoogleAuthProvider();
 
-function* loginSaga() {
-  const data = yield call(() => {
-    return authService.signInWithPopup(provider);
-  });
-  yield postAxios(process.env.REACT_APP_SERVER_URL + '/auth/login', data.user);
-  yield put(loginSuccess(data.user));
+function* loginSaga(action) {
+  if (action.payload) {
+    const userDbData = yield call(() => {
+      return getAxios(
+        process.env.REACT_APP_SERVER_URL +
+          '/users/' +
+          action.payload.userAuth.uid,
+      );
+    });
+
+    yield put(loginSuccess(userDbData.data));
+  } else {
+    const authData = yield call(() => {
+      return authService.signInWithPopup(provider);
+    });
+
+    yield postAxios(
+      process.env.REACT_APP_SERVER_URL + '/auth/login',
+      authData.user,
+    );
+
+    const userDbData = yield call(() => {
+      return getAxios(
+        process.env.REACT_APP_SERVER_URL + '/users/' + authData.user.uid,
+      );
+    });
+
+    yield put(loginSuccess(userDbData.data));
+  }
 }
 
 function* logoutSaga() {
@@ -23,10 +52,25 @@ function* logoutSaga() {
   yield put(logoutSuccess());
 }
 
+function* changeSettingSaga(action) {
+  yield patchAxios(
+    process.env.REACT_APP_SERVER_URL + '/users/' + action.payload.id,
+    {
+      id: action.payload.id,
+      selectedLanguage: action.payload.selectedLanguageSetting,
+      soundEffects: action.payload.soundEffectsSetting,
+    },
+  );
+}
+
 export function* watchLogin() {
   yield takeLatest(login, loginSaga);
 }
 
 export function* watchLogout() {
   yield takeLatest(logout, logoutSaga);
+}
+
+export function* watchChangeSetting() {
+  yield takeLatest(changeSetting, changeSettingSaga);
 }
