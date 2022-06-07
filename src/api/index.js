1357b83e7703609f3921091993dc421b27d31fc3
dongtimes2 <dongtimes2@gmail.com>
2022-06-07 22:48:00 +0900

    refector: 인가 작업을 서버가 수행할 때 이에 대응하여 프론트 로직이 제대로 구현되도록 리펙토링함

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,7 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
 });
 
-export const setAxios = async (user) => {
-  const token = await user?.getIdToken();
+export const setAxios = async (token) => {
   instance.defaults.headers.common['authorization'] = token;
 };
 
