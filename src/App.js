dad7601c25c09d2550e4683f6d300742cd6d30cd
dongtimes2 <dongtimes2@gmail.com>
2022-06-08 20:32:43 +0900

    feat: 새로고침 발생시 App.js에서 새로고침 액션함수를 실행. 새로고침 되는 동안 topbar의 버튼이 뜨지 않고 spinner가 뜨도록 변경

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import React, { useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
-import { setAxios } from './api';
 import './App.scss';
-import { checkAuthChanged } from './auth';
 import Layout from './components/Layout';
-import { loadUserDbData } from './features/userSlice';
+import { startRefresh } from './features/userSlice';
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const setInitialState = async () => {
-      const userAuth = await checkAuthChanged();
-
-      setAxios(await userAuth?.getIdToken());
-      userAuth && dispatch(loadUserDbData({ userAuth }));
-    };
-
-    setInitialState();
+    dispatch(startRefresh());
   }, []);
 
   return (
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import { login, logout } from '../features/userSlice';
 
 import Button from './Button';
+import Spinner from './Spinner';
 import styles from './Topbar.module.scss';
 
 const topbar = classNames.bind(styles);
@@ -16,6 +17,10 @@ export default function Topbar() {
   const userId = useSelector((state) => state.user.uid);
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   const isPracticing = useSelector((state) => state.user.isPracticing);
+  const isUserDataLoading = useSelector(
+    (state) => state.user.isUserDataLoading,
+  );
+  const isRefreshing = useSelector((state) => state.user.isRefreshing);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -42,7 +47,9 @@ export default function Topbar() {
           </span>
         </div>
         <div className={topbar('topbar__right')}>
-          {isLoggedIn ? (
+          {isUserDataLoading || isRefreshing ? (
+            <Spinner />
+          ) : isLoggedIn ? (
             <div className={topbar('topbar__right')}>
               {!isPracticing && (
                 <Button onClick={handleMyPageButtonClick}>My Page</Button>
