77973b019b11340bc91be828a4acbb858dee081a
dongtimes2 <dongtimes2@gmail.com>
2022-06-09 15:49:41 +0900

    feat: 결과 모달 창 구현

--- src/pages/WordPracticePage.js
+++ src/pages/WordPracticePage.js
@@ -1,15 +1,26 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 
 import classNames from 'classnames/bind';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../components/Button';
+import Modal from '../components/Modal';
+import { finishPractice } from '../features/userSlice';
+import ModalPortal from '../ModalPortal';
 
 import styles from './WordPracticePage.module.scss';
 
 const cx = classNames.bind(styles);
 
 export default function WordPracticePage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const wordList = useSelector((state) => state.problem.wordList);
+  const name = useSelector((state) => state.user.name);
+  const isPracticing = useSelector((state) => state.user.isPracticing);
 
   const [question, setQuestion] = useState('');
   const [questionLength, setQuestionLength] = useState(0);
@@ -23,11 +34,21 @@ export default function WordPracticePage() {
 
   const [attemptCount, setAttemptCount] = useState(0);
 
+  const [isShowing, setIsShowing] = useState(false);
+  const [isEnded, setIsEnded] = useState(false);
+
   const textInput = useRef(null);
 
   useEffect(() => {
     nextQuestion();
   }, [wordList]);
+
+  useMemo(() => {
+    if (attemptCount === 10) {
+      setIsShowing(true);
+      setIsEnded(true);
+    }
+  }, [attemptCount]);
 
   const keyBoardButton = {
     spaceBar: 32,
@@ -86,6 +107,12 @@ export default function WordPracticePage() {
     }
   };
 
+  const handleButtonClick = () => {
+    setIsShowing(false);
+    dispatch(finishPractice());
+    navigate('/');
+  };
+
   return (
     <div className={cx('container')}>
       <h1>낱말 연습</h1>
@@ -103,24 +130,40 @@ export default function WordPracticePage() {
           type="text"
           value={currentInput}
           onChange={handleChange}
+          disabled={isEnded}
         />
       </div>
       <p>맞은 횟수{correctCount}</p>
       <p>틀린 횟수{incorrectCount}</p>
       <p>도전 횟수{attemptCount}</p>
 
-      {
-        <div className={cx('section')}>
-          <div className="columns">
-            <div className="">
-              <div className="">Accuracy :</div>
-              <p className="">
-                {Math.round(((10 - incorrectCount) / 10) * 100)} %
-              </p>
-            </div>
+      <div className={cx('section')}>
+        <div className="columns">
+          <div className="">
+            <div className="">Accuracy :</div>
+            <p className="">
+              {Math.round(((10 - incorrectCount) / 10) * 100)} %
+            </p>
           </div>
         </div>
-      }
+      </div>
+      {isShowing && (
+        <ModalPortal>
+          <Modal
+            message={
+              <div>
+                <h1>낱말 연습 결과</h1>
+                <p>{name} 님의 연습 결과</p>
+                <p>
+                  정확도: {Math.round(((10 - incorrectCount) / 10) * 100)} %
+                </p>
+                <Button onClick={handleButtonClick}>홈으로 이동하기</Button>
+              </div>
+            }
+            onCloseModal={setIsShowing}
+          />
+        </ModalPortal>
+      )}
     </div>
   );
 }
