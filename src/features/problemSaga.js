dbb0ea713e3a9d199be0acb35a24f0c7b8b36cfd
chlugsdh58 <chlgusdh58@gmail.com>
2022-06-11 16:47:18 +0900

    feat: paragraph 문제를 saga를 이용해 불러오는 로직 구현

--- src/features/problemSaga.js
+++ src/features/problemSaga.js
@@ -3,6 +3,8 @@ import { put, takeLatest, call, delay, take } from 'redux-saga/effects';
 import { axiosGetRequest } from '../api';
 
 import {
+  getParagraphList,
+  getParagraphListSuccess,
   getSentenceList,
   getSentenceListSucces,
   getWordList,
@@ -41,10 +43,30 @@ function* getSentenceListSaga(action) {
   yield put(getSentenceListSucces(result.data[0][language]));
 }
 
+function* getParagraphListSaga(action) {
+  const language = action.payload.languages;
+  const practiceType = action.payload.types;
+
+  const result = yield call(() => {
+    return axiosGetRequest(
+      process.env.REACT_APP_SERVER_URL + '/languages/' + language,
+      {
+        params: { type: practiceType },
+      },
+    );
+  });
+
+  yield put(getParagraphListSuccess(result.data));
+}
+
 export function* watchGetWordList() {
   yield takeLatest(getWordList, getWordListSaga);
 }
 
 export function* watchGetSentenceList() {
   yield takeLatest(getSentenceList, getSentenceListSaga);
+}
+
+export function* watchGetParagraphList() {
+  yield takeLatest(getParagraphList, getParagraphListSaga);
 }
