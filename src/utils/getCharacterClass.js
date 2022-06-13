2e7aa687cb8cdf50633a5bd01b41113cac4099ab
rkdalstjd224 <rkdalstjd224@gmail.com>
2022-06-13 13:52:07 +0900

    feat: 문제 커서위치 보여지게 하기 구현중

--- src/utils/getCharacterClass.js
+++ src/utils/getCharacterClass.js
@@ -1,8 +1,12 @@
 export default function getCharacterClass(currentInput, index, character) {
+  const currentQuestionId = document.getElementById(index);
+  console.log(currentQuestionId);
   if (index < currentInput.split('').length) {
     if (character === currentInput.split('')[index]) {
+      currentQuestionId.classList.remove('current');
       return 'correct';
     } else {
+      currentQuestionId.classList.add('current');
       return 'wrong';
     }
   }
