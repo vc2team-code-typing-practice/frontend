6af05c0f34975504638fa6bb5968f7a1bcdbadfd
dongtimes2 <dongtimes2@gmail.com>
2022-06-16 15:41:20 +0900

    feat: 연습이 진행중일 때는 로고를 클릭시, 나갈 것을 묻는 모달이 뜨며, 연습을 하고 있지 않을 때에는 로고를 클릭시 바로 홈 화면으로 나가도록 구현

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classNames from 'classnames/bind';
 import { FaFileCode } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-import { login, logout, anonymousLogin } from '../features/userSlice';
+import {
+  login,
+  logout,
+  anonymousLogin,
+  unsetPracticeMode,
+} from '../features/userSlice';
+import ModalPortal from '../ModalPortal';
 
 import Button from './Button';
+import Modal from './Modal';
 import Spinner from './Spinner';
 import styles from './Topbar.module.scss';
 
 const topbar = classNames.bind(styles);
 
 export default function Topbar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const userId = useSelector((state) => state.user.uid);
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   const isPracticing = useSelector((state) => state.user.isPracticing);
@@ -23,8 +33,7 @@ export default function Topbar() {
   const isRefreshing = useSelector((state) => state.user.isRefreshing);
   const isAnonymousUser = useSelector((state) => state.user.isAnonymousUser);
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const [isShowingModal, setIsShowingModal] = useState(false);
 
   const handleLoginButtonClick = () => {
     dispatch(login());
@@ -37,18 +46,34 @@ export default function Topbar() {
   const handleLogoutButtonClick = () => {
     dispatch(logout());
   };
+
+  const handleLogoClick = () => {
+    if (isPracticing) {
+      setIsShowingModal(true);
+    } else {
+      navigate('/');
+    }
+  };
+
   const handleMyPageButtonClick = () => {
     navigate(`/users/${userId}`);
+  };
+
+  const handleGoBackHomeButtonClick = () => {
+    setIsShowingModal(false);
+    dispatch(unsetPracticeMode());
+    navigate('/');
+  };
+
+  const handleResumePracticeButtonClick = () => {
+    setIsShowingModal(false);
   };
 
   return (
     <div className={topbar('topbar')}>
       <div className={topbar('topbar__wrapper')}>
         <div className={topbar('topbar__left')}>
-          <span
-            className={topbar('topbar__logo')}
-            onClick={() => navigate('/')}
-          >
+          <span className={topbar('topbar__logo')} onClick={handleLogoClick}>
             <FaFileCode /> Code Typing Practice
           </span>
         </div>
@@ -72,6 +97,24 @@ export default function Topbar() {
           )}
         </div>
       </div>
+      {isShowingModal && (
+        <ModalPortal>
+          <Modal
+            message={
+              <div>
+                <p>
+                  정말로 연습을 종료하시겠습니까? <br />
+                </p>
+                <div>
+                  <Button onClick={handleGoBackHomeButtonClick}>Yes</Button>
+                  <Button onClick={handleResumePracticeButtonClick}>No</Button>
+                </div>
+              </div>
+            }
+            onCloseModal={setIsShowingModal}
+          ></Modal>
+        </ModalPortal>
+      )}
     </div>
   );
 }
