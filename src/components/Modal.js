6614e28dcde743de27cb04e4640a8da948c8a5c4
dongtimes2 <dongtimes2@gmail.com>
2022-06-16 15:43:35 +0900

    feat: 모달의 x버튼이나 모달 외부의 배경을 클릭시, 특정 url로 redirect 할 수 있도록 구현함

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import './Modal.scss';
 
-export default function Modal({ message, onCloseModal }) {
+export default function Modal({ message, onCloseModal, redirectLink }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    onCloseModal(false);
+
+    if (redirectLink) {
+      navigate(redirectLink);
+    }
+  };
+
   return (
-    <div className="modal__background" onClick={() => onCloseModal(false)}>
-      <div className="modal__box" onClick={(e) => e.stopPropagation()}>
-        <button
-          className="modal__closeModalBtn"
-          onClick={() => onCloseModal(false)}
-        >
+    <div className="modal__background" onClick={handleClick}>
+      <div className="modal__box" onClick={(event) => event.stopPropagation()}>
+        <button className="modal__closeModalBtn" onClick={handleClick}>
           X
         </button>
         <div className="modal__content">{message}</div>
